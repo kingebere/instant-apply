593da22e54254d06602e4f2eeea0cb87bb1f091a
codewarsfx <chiderainnocent001@gmail.com>
2023-06-06 14:24:42 +0100

    refactor code

--- instant.js
+++ instant.js
@@ -1,236 +1,266 @@
-//triggers  immediately after the page has been loaded
+class MainScript {
+	constructor() {
+		this.configureApp();
+		this.btn = null;
+		this.token = null;
+		this.phoneElement = document.querySelector(
+			"input[name='job_application[phone]']"
+		);
+		this.lastNameElement = document.querySelector(
+			"input[name='job_application[last_name]']"
+		);
+		this.firstNameElement = document.querySelector(
+			"input[name='job_application[first_name]']"
+		);
+		this.emailElement = document.querySelector(
+			"input[name='job_application[email]']"
+		);
+	}
 
-window.onload = () => {
-  //adds the pdf to the file input field
+	//handle when popbutton is clicked
+	async handlePopUpbuttonClicked() {
+		const token = (await chrome.storage.sync.get("accesstoken"))["accesstoken"];
 
-  var btn = document.createElement("div")
-  btn.classList.add("action_button")
+		if (token) {
+			const response = await fetch("https://instantapply.co/api/getUser", {
+				method: "GET",
+				mode: "cors",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+			});
 
-  var img = document.createElement("img")
-  img.src = "https://instantapply.co/assets/images/Instantapply-logo.png"
-  img.style.width = "50px"
-  img.style.height = "50px"
-  btn.appendChild(img)
+			if (response.status === 401)
+				window.open("https://instantapply.co", "_blank");
 
-  //styling the button
-  img.style.position = "fixed"
-  img.style.top = "10%"
-  img.style.right = "7%"
-  // btn.style.height = "50px";
-  // btn.style.width = "50px";
-  // btn.style.color = "white";
-  // btn.style.fontSize = "22px";
-  // btn.style.fontWeight = "bold";
-  // btn.style.backgroundColor = "#999";
-  // btn.style.borderRadius = "50%";
-  // btn.style.display = "flex";
-  // btn.style.alignItems = "center";
-  // btn.style.justifyContent = "center";
+			if (response.status === 200) {
+				this.data = await response.json();
+				const {
+					data: {
+						lastname,
+						linkedin,
+						gender,
+						youlatino,
+						firstname,
+						phone,
+						resume_email,
+						resume_url,
+						disabilitystatus,
+						veteranstatus,
+						github,
+						filename
+					},
+				} = this.data;
 
-  btn.style.cursor = "pointer"
+				this.updateNamePhoneEmail(firstname, lastname, phone, resume_email);
+				// Call the function to update the Github input value
+				this.updateGithubInputValue(github);
+				// Call the function to update the Linkedin input value
+				this.updateLinkedinInputValue(linkedin);
+				// Call the function to update the DOM
+				this.updateGender(gender);
+				// Call the function to update the DOM
+				this.youLatino(youlatino);
+				// Call the function to update the DOM
+				this.updateVeteranStatus(veteranstatus);
+				// Call the function to update the DOM
+				this.disabilityStatus(disabilitystatus);
+				this.uploadResume(resume_url,filename);
+			}
+		} else window.open("https://instantapply.co", "_blank");
+	}
 
-  document.body.appendChild(btn)
+	addPopUpButtonToPage() {
+		var btn = document.createElement("div");
+		btn.classList.add("action_button");
+		var img = document.createElement("img");
+		img.src = "https://instantapply.co/assets/images/Instantapply-logo.png";
+		img.style.width = "50px";
+		img.style.height = "50px";
+		btn.appendChild(img);
+		//styling the button
+		img.style.position = "fixed";
+		img.style.top = "10%";
+		img.style.right = "7%";
+		// btn.style.height = "50px";
+		// btn.style.width = "50px";
+		// btn.style.color = "white";
+		// btn.style.fontSize = "22px";
+		// btn.style.fontWeight = "bold";
+		// btn.style.backgroundColor = "#999";
+		// btn.style.borderRadius = "50%";
+		// btn.style.display = "flex";
+		// btn.style.alignItems = "center";
+		// btn.style.justifyContent = "center";
+		btn.style.cursor = "pointer";
+		document.body.appendChild(btn);
+		this.btn = btn;
+		this.configurePopUpButton();
+	}
 
-  async function addFiles() {
-    const token = (await chrome.storage.sync.get("accesstoken"))["accesstoken"]
+	//add event listener to popup button
+	configurePopUpButton() {
+		this.btn.addEventListener(
+			"click",
+			this.handlePopUpbuttonClicked.bind(this)
+		);
+	}
 
-    if (token) {
-      const response = await fetch("https://instantapply.co/api/getUser", {
-        method: "GET",
-        mode: "cors",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      })
-      const data = await response.json()
+	//add event listener for when page loads
+	configureApp() {
+		window.onload = this.addPopUpButtonToPage();
+	}
 
-      console.log("data", data)
-      const phone = document.querySelector(
-        "input[name='job_application[phone]']"
-      )
-      const lastName = document.querySelector(
-        "input[name='job_application[last_name]']"
-      )
-      const firstName = document.querySelector(
-        "input[name='job_application[first_name]']"
-      )
-      const email = document.querySelector(
-        "input[name='job_application[email]']"
-      )
+	updateNamePhoneEmail(firstName, lastName, email, phone) {
+		this.firstNameElement.value = firstName && firstName;
+		this.lastNameElement.value = lastName && lastName;
+		this.emailElement.value = email && email;
+		this.phoneElement.value = phone && phone;
+	}
 
-      // phone.value = data.data.phone &&  data.data.phone;
-      lastName.value = data.data.lastname && data.data.lastname
-      firstName.value = data.data.firstname && data.data.firstname
-      phone.value = data.data.phone && data.data.phone
-      email.value = data.data.resume_email && data.data.resume_email
+	updateGithubInputValue(github) {
+		// Get the div elements containing the labels
+		const divElements = document.querySelectorAll("#custom_fields > .field");
 
-      //update Github
-      function updateGithubInputValue() {
-        // Get the div elements containing the labels
-        const divElements = document.querySelectorAll("#custom_fields > .field")
+		// Find the div element with the "Github" label
+		let githubDiv;
+		for (const divElement of divElements) {
+			const label = divElement.querySelector("label");
+			if (label && label.textContent.includes("Github")) {
+				githubDiv = divElement;
+				break;
+			}
+		}
 
-        // Find the div element with the "Github" label
-        let githubDiv
-        for (const divElement of divElements) {
-          const label = divElement.querySelector("label")
-          if (label && label.textContent.includes("Github")) {
-            githubDiv = divElement
-            break
-          }
-        }
+		// If the "Github" div element is found, update the input value
+		if (githubDiv) {
+			const input = githubDiv.querySelector('input[type="text"]');
+			if (input) {
+				const updatedValue = github;
+				input.value = updatedValue;
+			}
+		}
+	}
+	//update LinkedIn
+	updateLinkedinInputValue(linkedin) {
+		// Get the div elements containing the labels
+		const divElements = document.querySelectorAll("#custom_fields > .field");
 
-        // If the "Github" div element is found, update the input value
-        if (githubDiv) {
-          const input = githubDiv.querySelector('input[type="text"]')
-          if (input) {
-            const updatedValue = data.data.github
-            input.value = updatedValue
-          }
-        }
-      }
+		// Find the div element with the "Linkedin" label
+		let linkedinDiv;
+		for (const divElement of divElements) {
+			const label = divElement.querySelector("label");
+			if (label && label.textContent.includes("LinkedIn")) {
+				linkedinDiv = divElement;
+				break;
+			}
+		}
 
-      // Call the function to update the Github input value
-      updateGithubInputValue()
+		// If the "LinkedIn" div element is found, update the input value
+		if (linkedinDiv) {
+			const input = linkedinDiv.querySelector('input[type="text"]');
+			if (input) {
+				const updatedValue = linkedin;
+				input.value = updatedValue;
+			}
+		}
+	}
 
-      //update LinkedIn
-      function updateLinkedinInputValue() {
-        // Get the div elements containing the labels
-        const divElements = document.querySelectorAll("#custom_fields > .field")
+	//add gender
+	updateGender(gender) {
+		// Find the parent element with the ID "s2id_job_application_gender"
+		const parentElement = document.getElementById(
+			"s2id_job_application_gender"
+		);
 
-        // Find the div element with the "Linkedin" label
-        let linkedinDiv
-        for (const divElement of divElements) {
-          const label = divElement.querySelector("label")
-          if (label && label.textContent.includes("LinkedIn")) {
-            linkedinDiv = divElement
-            break
-          }
-        }
+		// If the parent element is found
+		if (parentElement) {
+			// Find the child element with the class name "select2-chosen"
+			const select2ChosenElement =
+				parentElement.querySelector(".select2-chosen");
 
-        // If the "LinkedIn" div element is found, update the input value
-        if (linkedinDiv) {
-          const input = linkedinDiv.querySelector('input[type="text"]')
-          if (input) {
-            const updatedValue = data.data.linkedin
-            input.value = updatedValue
-          }
-        }
-      }
+			// Update the child element's text content to a Selected Gender
+			select2ChosenElement.textContent = gender;
+		}
+	}
 
-      // Call the function to update the Linkedin input value
-      updateLinkedinInputValue()
+	//add "are you a latino"
+	youLatino(youlatino) {
+		// Find the parent element with the ID "  s2id_job_application_hispanic_ethnicity"
+		const parentElement = document.getElementById(
+			"s2id_job_application_hispanic_ethnicity"
+		);
 
-      //add gender
-      function updateGender() {
-        // Find the parent element with the ID "s2id_job_application_gender"
-        const parentElement = document.getElementById(
-          "s2id_job_application_gender"
-        )
+		// If the parent element is found
+		if (parentElement) {
+			// Find the child element with the class name "select2-chosen"
+			const select2ChosenElement =
+				parentElement.querySelector(".select2-chosen");
 
-        // If the parent element is found
-        if (parentElement) {
-          // Find the child element with the class name "select2-chosen"
-          const select2ChosenElement =
-            parentElement.querySelector(".select2-chosen")
+			// Update the child element's text content to a boolean
+			select2ChosenElement.textContent = youlatino;
+		}
+	}
+	updateVeteranStatus(veteranstatus) {
+		// Find the parent element with the ID "s2id_job_application_veteran_status"
+		const parentElement = document.getElementById(
+			"s2id_job_application_veteran_status"
+		);
 
-          // Update the child element's text content to a Selected Gender
-          select2ChosenElement.textContent = data.data.gender
-        }
-      }
+		// If the parent element is found
+		if (parentElement) {
+			// Find the child element with the class name "select2-chosen"
+			const select2ChosenElement =
+				parentElement.querySelector(".select2-chosen");
 
-      // Call the function to update the DOM
-      updateGender()
+			// Update the child element's text content
+			select2ChosenElement.textContent = veteranstatus;
+		}
+	}
+	//add veteran status
+	disabilityStatus(disabilitystatus) {
+		// Find the parent element with the ID "s2id_job_application_disability_status"
+		const parentElement = document.getElementById(
+			"s2id_job_application_disability_status"
+		);
 
-      //add "are you a latino"
-      function youLatino() {
-        // Find the parent element with the ID "  s2id_job_application_hispanic_ethnicity"
-        const parentElement = document.getElementById(
-          "s2id_job_application_hispanic_ethnicity"
-        )
+		// If the parent element is found
+		if (parentElement) {
+			// Find the child element with the class name "select2-chosen"
+			const select2ChosenElement =
+				parentElement.querySelector(".select2-chosen");
 
-        // If the parent element is found
-        if (parentElement) {
-          // Find the child element with the class name "select2-chosen"
-          const select2ChosenElement =
-            parentElement.querySelector(".select2-chosen")
+			// Update the child element's text content to a boolean
+			select2ChosenElement.textContent = disabilitystatus;
+		}
+	}
 
-          // Update the child element's text content to a boolean
-          select2ChosenElement.textContent = data.data.youlatino
-        }
-      }
+	async uploadResume(resume_url,filename) {
+		// website.value = data.data.website && data.data.website;
+		const yes = await fetch(resume_url, {
+			method: "GET",
+			mode: "cors",
+		});
 
-      // Call the function to update the DOM
-      youLatino()
+		const red = await yes.blob();
 
-      function updateVeteranStatus() {
-        // Find the parent element with the ID "s2id_job_application_veteran_status"
-        const parentElement = document.getElementById(
-          "s2id_job_application_veteran_status"
-        )
+		const myFile = new File([red], filename, {
+			type: "application/pdf",
+			lastModified: new Date(),
+		});
 
-        // If the parent element is found
-        if (parentElement) {
-          // Find the child element with the class name "select2-chosen"
-          const select2ChosenElement =
-            parentElement.querySelector(".select2-chosen")
+		const dataTransfer = new DataTransfer();
+		dataTransfer.items.add(myFile);
+		const dec = document.querySelector(
+			"#s3_upload_for_resume input[type='file']"
+		);
+		dec.files = dataTransfer.files;
 
-          // Update the child element's text content
-          select2ChosenElement.textContent = data.data.veteranstatus
-        }
-      }
+		const event = new Event("change");
 
-      // Call the function to update the DOM
-      updateVeteranStatus()
-
-      //add veteran status
-      function disabilityStatus() {
-        // Find the parent element with the ID "s2id_job_application_disability_status"
-        const parentElement = document.getElementById(
-          "s2id_job_application_disability_status"
-        )
-
-        // If the parent element is found
-        if (parentElement) {
-          // Find the child element with the class name "select2-chosen"
-          const select2ChosenElement =
-            parentElement.querySelector(".select2-chosen")
-
-          // Update the child element's text content to a boolean
-          select2ChosenElement.textContent = data.data.disabilitystatus
-        }
-      }
-
-      // Call the function to update the DOM
-      disabilityStatus()
-
-      // website.value = data.data.website && data.data.website;
-      const yes = await fetch(data.data.resume_url, {
-        method: "GET",
-        mode: "cors",
-      })
-
-      const red = await yes.blob()
-
-      const myFile = new File([red], data.data.filename, {
-        type: "application/pdf",
-        lastModified: new Date(),
-      })
-
-      const dataTransfer = new DataTransfer()
-      dataTransfer.items.add(myFile)
-      const dec = document.querySelector(
-        "#s3_upload_for_resume input[type='file']"
-      )
-      dec.files = dataTransfer.files
-
-      const event = new Event("change")
-
-      dec.dispatchEvent(event)
-    } else window.open("https://instantapply.co", "_blank")
-  }
-
-  btn.addEventListener("click", function () {
-    addFiles()
-  })
+		dec.dispatchEvent(event);
+	}
 }
+
+new MainScript();
