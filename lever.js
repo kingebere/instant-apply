2a2ee2ef2c6277ca86f0c8effb0a1da1eb9d766b
codewarsfx <chiderainnocent001@gmail.com>
2023-06-24 13:30:18 +0100

    feat:modal ui

--- lever.js
+++ lever.js
@@ -1,97 +1,214 @@
 // Util function for getting the name of the company being applied to
 function getCompanyName() {
-  this.presentWindowUrl = window.location.href
-  return this.presentWindowUrl.split("/")[3]
+	this.presentWindowUrl = window.location.href;
+	return this.presentWindowUrl.split("/")[3];
 }
 
 class leverMainScript {
-  constructor() {
-    this.configureApp()
-    this.btn = null
-    this.token = null
-    this.fullNameElement = document.querySelector("input[name='name']")
-    this.emailElement = document.querySelector("input[name='email']")
-    this.phoneElement = document.querySelector("input[name='phone']")
-    this.companyElement = document.querySelector("input[name='org']")
-    this.linkedinElement = document.querySelector(
-      "input[name='urls[LinkedIn]']"
-    )
-    this.jobPositionElement = document.querySelector(".posting-header h2")
-    this.locationElement = document.querySelector(".posting-category.location")
-    this.commitmentElement = document.querySelector("div.commitment")
-    this.formElement = document.querySelector("#application-form")
-    this.configureForm()
-  }
+	constructor() {
+		this.configureApp();
+		this.btn = null;
+		this.token = null;
+		this.fullNameElement = document.querySelector("input[name='name']");
+		this.emailElement = document.querySelector("input[name='email']");
+		this.phoneElement = document.querySelector("input[name='phone']");
+		this.companyElement = document.querySelector("input[name='org']");
+		this.linkedinElement = document.querySelector(
+			"input[name='urls[LinkedIn]']"
+		);
+		this.jobPositionElement = document.querySelector(".posting-header h2");
+		this.locationElement = document.querySelector(".posting-category.location");
+		this.commitmentElement = document.querySelector("div.commitment");
+		this.formElement = document.querySelector("#application-form");
+		this.addSubscribeModal();
 
-  async handleFormSubmit(e) {
-    // Construct job details and save on Chrome storage when user submits
-    const {
-      data: {
-        session: {
-          user: { id },
-        },
-      },
-    } = JSON.parse((await chrome.storage.sync.get("session"))["session"])
 
-    const jobDescription = {
-      position: this.jobPositionElement.textContent,
-      companyApplied: getCompanyName(),
-      jobBoard: "lever",
-      applicationUrl: window.location.href,
-      location: this.locationElement.textContent.slice(0, -1),
-      commitment: this.commitmentElement.textContent,
-      userID: id,
-    }
+	}
 
-    chrome.storage.sync.set(
-      { jobDescription: JSON.stringify(jobDescription) },
-      function () {}
-    )
-  }
+	removeSubscribeModal() {
+		const modalContainer = document.body.querySelector(".modal-container");
+		document.body.removeChild(modalContainer);
+	}
 
-  configureForm() {
-    this.formElement.addEventListener(
-      "submit",
-      this.handleFormSubmit.bind(this)
-    )
-  }
+	addSubscribeModal() {
+		// Create the modal container
+		var modalContainer = document.createElement("div");
+		modalContainer.className = "modal-container";
+		modalContainer.setAttribute(
+			"style",
+			`position: fixed;
+		z-index: 9999;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		display:flex;
+		z-indeX:100000;
+		background-color: rgba(0, 0, 0, 0.5);
+		 justify-content: center;
+		 align-items: center;`
+		);
 
-  fillnameEmailCompanyLinkedIn(
-    fullName,
-    email,
-    phone,
-    currentCompany,
-    linkedin
-  ) {
-    this.fullNameElement.value = fullName && fullName
-    this.emailElement.value = email && email
-    this.phoneElement.value = phone && phone
-    this.companyElement.value = currentCompany && currentCompany
-    this.linkedinElement.value = linkedin
-  }
+		// Create the modal content
+		var modalContent = document.createElement("div");
+		modalContent.setAttribute(
+			"style",
+			` background-color: #fefefe;
+			position:relative;
+		display:flex;
+		flex-direction: column;
+		align-items:center;
+		padding:1em;
+		border: 1px solid #888;
+		border-radius:.5em;
+		height:300px;
+		width: 90%;
+		max-width: 500px;`
+		);
 
-  async uploadResume(resume_url, filename) {
-    const getPDF = await fetch(resume_url, {
-      method: "GET",
-      mode: "cors",
-    })
+		// Create the close button
+		var closeButton = document.createElement("span");
+		closeButton.innerHTML = "&times;";
 
-    const pdfBlob = await getPDF.blob()
+		closeButton.setAttribute(
+			"style",
+			`
+		color: #aaa;
+		position: absolute;
+		right:2%;
+		font-size: 28px;
+		font-weight: bold;
+		cursor: pointer;
+		`
+		);
 
-    const myFile = new File([pdfBlob], filename, {
-      type: "application/pdf",
-      lastModified: new Date(),
-    })
+		// Create the modal title
+		var modalTitle = document.createElement("h2");
+		modalTitle.textContent = "Upgrade for Limitless Auto fills";
+		modalTitle.style.marginBottom = "0";
 
-    const dataTransfer = new DataTransfer()
-    dataTransfer.items.add(myFile)
+		modalTitle.setAttribute(
+			"style",
+			`
+		text-align:center;
+		margin:1em .5em 0 0;
+		color: #666;
+		font-size: 28px;
+		font-weight: bold;
+		`
+		);
 
-    const dec = document.querySelector("#resume-upload-input")
-    dec.files = dataTransfer.files
+		// Create the modal body
+		var modalBody = document.createElement("a");
+		modalBody.textContent = "Subscribe Now to get Unlimited Fills";
+		modalBody.href = "https://instantapply.co/";
 
-    const event = new Event("change")
-    dec.dispatchEvent(event)
-  }
+		modalBody.setAttribute(
+			"style",
+			`
+		margin:0;
+		color:blue;
+		font-size: 18px;
+		text-decoration:underline;
+		`
+		);
+
+		var img = document.createElement("img");
+		img.src = "https://instantapply.co/assets/images/subscribe.png";
+
+		img.setAttribute(
+			"style",
+			`
+		width:150px;
+		height:90px;
+		display:inline-block;
+		marign: 0 auto;
+
+		`
+		);
+
+		// Append the elements to the modal content
+		modalContent.appendChild(closeButton);
+		modalContent.appendChild(modalTitle);
+		modalContent.appendChild(img);
+		modalContent.appendChild(modalBody);
+
+		// Append the modal content to the modal container
+		modalContainer.appendChild(modalContent);
+
+		// Append the modal container to the document body
+		document.body.appendChild(modalContainer);
+	}
+
+	async handleFormSubmit(e) {
+		// Construct job details and save on Chrome storage when user submits
+		const {
+			data: {
+				session: {
+					user: { id },
+				},
+			},
+		} = JSON.parse((await chrome.storage.sync.get("session"))["session"]);
+
+		const jobDescription = {
+			position: this.jobPositionElement.textContent,
+			companyApplied: getCompanyName(),
+			jobBoard: "lever",
+			applicationUrl: window.location.href,
+			location: this.locationElement.textContent.slice(0, -1),
+			commitment: this.commitmentElement.textContent,
+			userID: id,
+		};
+
+		chrome.storage.sync.set(
+			{ jobDescription: JSON.stringify(jobDescription) },
+			function () {}
+		);
+	}
+
+	configureForm() {
+		this.formElement.addEventListener(
+			"submit",
+			this.handleFormSubmit.bind(this)
+		);
+	}
+
+	fillnameEmailCompanyLinkedIn(
+		fullName,
+		email,
+		phone,
+		currentCompany,
+		linkedin
+	) {
+		this.fullNameElement.value = fullName && fullName;
+		this.emailElement.value = email && email;
+		this.phoneElement.value = phone && phone;
+		this.companyElement.value = currentCompany && currentCompany;
+		this.linkedinElement.value = linkedin;
+	}
+
+	async uploadResume(resume_url, filename) {
+		const getPDF = await fetch(resume_url, {
+			method: "GET",
+			mode: "cors",
+		});
+
+		const pdfBlob = await getPDF.blob();
+
+		const myFile = new File([pdfBlob], filename, {
+			type: "application/pdf",
+			lastModified: new Date(),
+		});
+
+		const dataTransfer = new DataTransfer();
+		dataTransfer.items.add(myFile);
+
+		const dec = document.querySelector("#resume-upload-input");
+		dec.files = dataTransfer.files;
+
+		const event = new Event("change");
+		dec.dispatchEvent(event);
+	}
 
 	async fetchCount(userID) {
 		const response = await fetch(
@@ -160,37 +277,40 @@ class leverMainScript {
 		} else window.open("https://instantapply.co", "_blank");
 	}
 
-  addPopUpButtonToPage() {
-    var btn = document.createElement("div")
-    btn.classList.add("action_button")
-    var img = document.createElement("img")
-    img.src = "https://instantapply.co/assets/images/instantapply-logo.svg"
-    img.style.width = "50px"
-    img.style.height = "50px"
-    btn.appendChild(img)
+	addPopUpButtonToPage() {
+		var btn = document.createElement("div");
+		btn.classList.add("action_button");
+		var img = document.createElement("img");
+		img.src = "https://instantapply.co/assets/images/instantapply-logo.svg";
+		img.style.width = "50px";
+		img.style.height = "50px";
+		btn.appendChild(img);
 
-    // Styling the button
-    img.style.position = "fixed"
-    img.style.top = "10%"
-    img.style.right = "7%"
-    img.style.borderRadius = "8px"
-    img.style.backgroundColor = "#ede2ff"
-    img.style.padding = "10px"
-    img.style.zIndex = "999"
-    btn.style.cursor = "pointer"
+		// Styling the button
+		img.style.position = "fixed";
+		img.style.top = "10%";
+		img.style.right = "7%";
+		img.style.borderRadius = "8px";
+		img.style.backgroundColor = "#ede2ff";
+		img.style.padding = "10px";
+		img.style.zIndex = "999";
+		btn.style.cursor = "pointer";
 
-    document.body.appendChild(btn)
-    this.btn = btn
-    this.configurePopUpButton()
-  }
+		document.body.appendChild(btn);
+		this.btn = btn;
+		this.configurePopUpButton();
+	}
 
-  configurePopUpButton() {
-    this.btn.addEventListener("click", this.handlePopUpbuttonClicked.bind(this))
-  }
+	configurePopUpButton() {
+		this.btn.addEventListener(
+			"click",
+			this.handlePopUpbuttonClicked.bind(this)
+		);
+	}
 
-  configureApp() {
-    window.onload = this.addPopUpButtonToPage()
-  }
+	configureApp() {
+		window.onload = this.addPopUpButtonToPage();
+	}
 }
 
-new leverMainScript()
+new leverMainScript();
