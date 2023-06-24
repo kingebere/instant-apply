ec1c11c092514610df83db50a4dfbbc432b30ba3
kingebere <gudoski30@gmail.com>
2023-06-24 20:00:49 +0100

    feat:populate gmail

--- gmail.js
+++ gmail.js
@@ -1,50 +1,65 @@
 class gmailMainScript {
   constructor() {
-    this.configureApp();
-    this.btn = null;
-    this.token = null;
-    this.fullNameElement = document.querySelector("input[name='name']");
-    this.emailElement = document.querySelector("input[name='email']");
-    this.phoneElement = document.querySelector("input[name='phone']");
-    this.companyElement = document.querySelector("input[name='org']");
-    this.linkedinElement = document.querySelector("input[name='urls[LinkedIn]']");
+    this.configureApp()
+    this.btn = null
+    this.token = null
   }
 
-  fillnameEmailCompanyLinkedIn(fullName, email, phone, currentCompany, linkedin) {
-    this.fullNameElement.value = fullName && fullName;
-    this.emailElement.value = email && email;
-    this.phoneElement.value = phone && phone;
-    this.companyElement.value = currentCompany && currentCompany;
-    this.linkedinElement.value = linkedin;
+  fillnameEmailCompanyLinkedIn(
+    gmailRecipientEmail,
+    gmailSubject,
+    gmailContent
+  ) {
+    const hiddensubjectElement = document.querySelector(".fX.aiL")
+    const hiddensubjectElement2 = document.querySelector(".aoD.hl")
+    const gmailSubjectElement = document.querySelector(
+      "input[name='subjectbox']"
+    )
+
+    const emailElement = document.querySelector("input[class='agP aFw']")
+    const emailElementField = document.querySelector(".aIa.aFw")
+
+    //hides the recipient div
+    hiddensubjectElement2.style.display = "none"
+    //shows the targeted input
+    if (hiddensubjectElement) {
+      hiddensubjectElement.style.display = "block"
+    }
+
+    gmailSubjectElement.value = gmailSubject && gmailSubject
+
+    emailElement.value = gmailRecipientEmail && gmailRecipientEmail
+    emailElementField.value = gmailRecipientEmail && gmailRecipientEmail
+    console.log(gmailContent, "text")
   }
 
   async uploadResume(resume_url, filename) {
     const getPDF = await fetch(resume_url, {
       method: "GET",
       mode: "cors",
-    });
+    })
 
-    const pdfBlob = await getPDF.blob();
+    const pdfBlob = await getPDF.blob()
 
     const myFile = new File([pdfBlob], filename + ".pdf", {
       type: "application/pdf",
       lastModified: new Date(),
-    });
+    })
 
-    const dataTransfer = new DataTransfer();
-    dataTransfer.items.add(myFile);
-    const dec = document.querySelector('input[type="file"][name="Filedata"]');
-    dec.files = dataTransfer.files;
+    const dataTransfer = new DataTransfer()
+    dataTransfer.items.add(myFile)
+    const dec = document.querySelector('input[type="file"][name="Filedata"]')
+    dec.files = dataTransfer.files
 
-    const event = new Event("change");
-    dec.dispatchEvent(event);
+    const event = new Event("change")
+    dec.dispatchEvent(event)
   }
 
   async handlePopUpbuttonClicked() {
-    const session = (await chrome.storage.sync.get("session"))["session"];
+    const session = (await chrome.storage.sync.get("session"))["session"]
 
     if (session) {
-      console.log(session);
+      console.log(session)
       const response = await fetch("https://instantapply.co/api/getUser", {
         method: "POST",
         mode: "cors",
@@ -54,70 +69,66 @@ class gmailMainScript {
         headers: {
           "Content-Type": "application/json",
         },
-      });
+      })
 
       if (response.status === 401)
-        window.open("https://instantapply.co", "_blank");
+        window.open("https://instantapply.co", "_blank")
 
       if (response.status === 200) {
-        const clickCompose = document.querySelector(".T-I.T-I-KE.L3");
-        clickCompose.click();
-        this.data = await response.json();
+        const clickCompose = document.querySelector(".T-I.T-I-KE.L3")
+        clickCompose.click()
+        this.data = await response.json()
+        console.log(this.data)
         const {
           data: {
-            lastname,
-            linkedin,
-            firstname,
-            phone,
-            resume_email,
+            gmailRecipientEmail,
+            gmailSubject,
+            gmailContent,
             resume_url,
             filename,
-            currentCompany,
           },
-        } = this.data;
+        } = this.data
+        this.uploadResume(resume_url, filename)
         this.fillnameEmailCompanyLinkedIn(
-          `${firstname} ${lastname}`,
-          resume_email,
-          phone,
-          currentCompany,
-          linkedin
-        );
-        this.uploadResume(resume_url, filename);
+          gmailRecipientEmail,
+          gmailSubject,
+          gmailContent
+        )
       }
-    } else window.open("https://instantapply.co", "_blank");
+    } else window.open("https://instantapply.co", "_blank")
   }
 
   addPopUpButtonToPage() {
-    var btn = document.createElement("div");
-    btn.classList.add("action_button");
-    var img = document.createElement("img");
-    img.src = "https://instantapply.co/assets/images/instantapply-logo.svg";
-    img.style.width = "50px";
-    img.style.height = "50px";
-    btn.appendChild(img);
+    var btn = document.createElement("div")
+    btn.classList.add("action_button")
+    var img = document.createElement("img")
+    img.src = "https://instantapply.co/assets/images/instantapply-logo.svg"
+    img.style.width = "50px"
+    img.style.height = "50px"
+    btn.appendChild(img)
     //styling the button
-    img.style.position = "fixed";
-    img.style.top = "10%";
-    img.style.right = "7%";
-    img.style.borderRadius = "8px";
-    img.style.backgroundColor = "#ede2ff";
-    img.style.padding = "10px";
-    img.style.zIndex = "999";
-    btn.style.cursor = "pointer";
-    document.body.appendChild(btn);
-    this.btn = btn;
-    this.configurePopUpButton();
+    img.style.position = "fixed"
+    img.style.top = "10%"
+    img.style.right = "7%"
+    img.style.borderRadius = "8px"
+    img.style.backgroundColor = "#ede2ff"
+    img.style.padding = "10px"
+    img.style.zIndex = "999"
+    btn.style.cursor = "pointer"
+    document.body.appendChild(btn)
+    this.btn = btn
+    this.configurePopUpButton()
   }
 
   configurePopUpButton() {
-    this.btn.addEventListener("click", this.handlePopUpbuttonClicked.bind(this));
+    this.btn.addEventListener("click", this.handlePopUpbuttonClicked.bind(this))
   }
 
   configureApp() {
     window.addEventListener("load", () => {
-      this.addPopUpButtonToPage();
-    });
+      this.addPopUpButtonToPage()
+    })
   }
 }
 
-new gmailMainScript();
+new gmailMainScript()
