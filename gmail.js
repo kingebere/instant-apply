adae097ba24d415b854f633a402073c80c4f25bc
kingebere <gudoski30@gmail.com>
2023-06-25 02:49:40 +0100

    feat:change positioing of logo

--- gmail.js
+++ gmail.js
@@ -1,20 +1,20 @@
 class gmailMainScript {
   constructor() {
-    this.configureApp()
+    // this.configureApp()
     this.btn = null
     this.token = null
 
-        // Listen for messages from the background script
-        chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-          if (message.url) {
-            // Perform actions based on the URL change
-            const url = message.url;
-            console.log('Performing actions for URL:', url);
-    
-            // Call a function or trigger the desired action
-            this.addPopUpButtonToPage();
-          }
-        });
+    // Listen for messages from the background script
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      if (message.url) {
+        // Perform actions based on the URL change
+        const url = message.url
+        console.log("Performing actions for URL:", url)
+
+        // Call a function or trigger the desired action
+        this.addPopUpButtonToPage()
+      }
+    })
   }
 
   fillnameEmailCompanyLinkedIn(
@@ -43,68 +43,61 @@ class gmailMainScript {
     // emailElement.value = gmailRecipientEmail && gmailRecipientEmail
     // emailElementField.value = gmailRecipientEmail && gmailRecipientEmail
 
-
     //This gets the email when the email inputed is highlighted by gmail
-    const element = document.querySelector('div.afV[data-hovercard-id]');
-    const content = element.getAttribute('data-hovercard-id');
-    
-    console.log(content); // Output: design@uiland.design
-    
+    const element = document.querySelector("div.afV[data-hovercard-id]")
+    const content = element.getAttribute("data-hovercard-id")
 
+    console.log(content) // Output: design@uiland.design
 
+    // Get the first div element
+    const targetDiv = document.querySelector("div.Am.Al.editable.LW-avf.tS-tW")
 
- // Get the first div element
-const targetDiv = document.querySelector('div.Am.Al.editable.LW-avf.tS-tW');
+    // Find the first child <div> element within the parent
+    const firstDiv = targetDiv.querySelector("div")
 
-// Find the first child <div> element within the parent
-const firstDiv = targetDiv.querySelector('div');
+    // Find the <br> tag inside the div
+    const brTag = firstDiv.querySelector("br")
 
-// Find the <br> tag inside the div
-const brTag = firstDiv.querySelector('br');
+    // Find the nearest parent of the <br> tag
+    const ancestorElement = brTag.parentElement
 
-// Find the nearest parent of the <br> tag
-const ancestorElement = brTag.parentElement;
+    // Create a <pre> element
+    const preTag = document.createElement("pre")
+    preTag.style.whiteSpace = "pre-wrap" // Preserve line breaks and spaces
+    preTag.style.font = "small/1.5 Arial,Helvetica,sans-serif"
+    preTag.style.letterSpacing = "normal"
 
-// Create a <pre> element
-const preTag = document.createElement('pre');
-preTag.style.whiteSpace = 'pre-wrap'; // Preserve line breaks and spaces
-preTag.style.font='small/1.5 Arial,Helvetica,sans-serif';
-preTag.style.letterSpacing='normal';
+    if (content || emailElement) {
+      console.log(content, emailElement)
+      // Email address
+      const emailAddress = content || emailElement
 
-if (content || emailElement) {
-  console.log(content, emailElement);
-  // Email address
-  const emailAddress = content || emailElement;
+      // Extract the text after the '@' symbol
+      const atIndex = emailAddress.indexOf("@")
+      const domain = emailAddress.substr(atIndex + 1)
 
-  // Extract the text after the '@' symbol
-  const atIndex = emailAddress.indexOf('@');
-  const domain = emailAddress.substr(atIndex + 1);
+      // Extract the text before the '.' symbol in the domain
+      const dotIndex = domain.indexOf(".")
+      const text = domain.substr(0, dotIndex)
 
-  // Extract the text before the '.' symbol in the domain
-  const dotIndex = domain.indexOf('.');
-  const text = domain.substr(0, dotIndex);
+      // Capitalize the first letter
+      const companyName = text.charAt(0).toUpperCase() + text.slice(1)
 
-  // Capitalize the first letter
-  const companyName = text.charAt(0).toUpperCase() + text.slice(1);
+      console.log(companyName)
 
-  console.log(companyName);
+      // Replace placeholder in gmailContent with companyName
+      const replacedContent = gmailContent.replace("{{company}}", companyName)
+      console.log(replacedContent)
 
-  // Replace placeholder in gmailContent with companyName
-  const replacedContent = gmailContent.replace("{{company}}", companyName);
-  console.log(replacedContent);
+      // Set the text content of the <pre> tag to the replaced content
+      preTag.textContent = replacedContent
+    } else {
+      // Set the text content of the <pre> tag to the original content
+      preTag.textContent = gmailContent
+    }
 
-  // Set the text content of the <pre> tag to the replaced content
-  preTag.textContent = replacedContent;
-} else {
-  // Set the text content of the <pre> tag to the original content
-  preTag.textContent = gmailContent;
-}
-
-// Replace the <br> tag with the <pre> tag
-ancestorElement.replaceChild(preTag, brTag);
-
-
-  
+    // Replace the <br> tag with the <pre> tag
+    ancestorElement.replaceChild(preTag, brTag)
   }
 
   async uploadResume(resume_url, filename) {
@@ -173,37 +166,48 @@ ancestorElement.replaceChild(preTag, brTag);
   }
 
   addPopUpButtonToPage() {
-    const currentUrl = window.location.href;
-    const targetUrl = "https://mail.google.com/mail/u/0/#inbox?compose=new";
-    
+    const currentUrl = window.location.href
+    const targetUrl = "https://mail.google.com/mail/u/0/#inbox?compose=new"
+
     if (currentUrl === targetUrl) {
       var btn = document.createElement("div");
       btn.classList.add("action_button");
+      
       var img = document.createElement("img");
       img.src = "https://instantapply.co/assets/images/instantapply-logo.svg";
-      img.style.width = "50px";
-      img.style.height = "50px";
+      img.style.width = "24px";
+      img.style.height = "24px";
       btn.appendChild(img);
-      //styling the button
-      img.style.position = "fixed";
+      
+      // Styling the button
       img.style.top = "10%";
       img.style.right = "7%";
       img.style.borderRadius = "8px";
       img.style.backgroundColor = "#ede2ff";
-      img.style.padding = "10px";
+      img.style.padding = "6px";
+      img.style.marginLeft = "6px";
       img.style.zIndex = "999";
       btn.style.cursor = "pointer";
-      document.body.appendChild(btn);
-      this.btn = btn;
-      this.configurePopUpButton();
+      
+      // Find the target element to append the button
+      var targetElement = document.querySelector("tr.btC");
+      var td = document.createElement("td");
+      td.appendChild(btn);
+      
+      // Get the first child (first td) in the row
+      var firstChild = targetElement.firstElementChild;
+      
+      // Insert the new td after the first child
+      firstChild.insertAdjacentElement("afterend", td);
+      
+      this.btn = td
+      this.configurePopUpButton()
     }
   }
-  
 
   configurePopUpButton() {
     this.btn.addEventListener("click", this.handlePopUpbuttonClicked.bind(this))
   }
-
 
   configureApp() {
     window.addEventListener("load", () => {
