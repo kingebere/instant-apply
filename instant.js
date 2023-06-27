1b161edb6d98d922a40824cdefc22261863bad64
codewarsfx <chiderainnocent001@gmail.com>
2023-06-27 22:36:34 +0100

    fix

--- instant.js
+++ instant.js
@@ -493,7 +493,7 @@ class greenhouseMainScript {
 			});
 
 			try {
-				const response = await fetch("https://instantapply.co/api/content", {
+				const response = await fetch("https://api.instantapply.co/content", {
 					method: "POST",
 					body: JSON.stringify({
 						session,
